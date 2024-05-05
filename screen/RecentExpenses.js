55592c76ed510669af4e5762d25165ead083cfc8
d.Khazaei <dariushkhazaei629@gmail.com>
2024-05-05 08:20:37 +0330

    handle recent expenses screen

---
+++ screen/RecentExpenses.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../uitls/date";
+
+export default function RecentExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  });
+
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No registered expenses found!"
+    />
+  );
+}
